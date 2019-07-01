c14b69abd2a7a3e7caadf29ab61a26df9abd133b
TokensPony <cmtherealmccoy@gmail.com>
2019-07-01 18:07:15 -0500

    Added cube animations and scripts. Adjusted "screen" height

---
+++ Assets/Scripts/CubeRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour {
+
+	public float xDegrees;
+	public float yDegrees;
+	public float zDegrees;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (xDegrees * Time.deltaTime, yDegrees * Time.deltaTime, zDegrees * Time.deltaTime);
+	}
+}
